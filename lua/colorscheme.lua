f9f1537f2a8047b2d70435c72028b03fa15336ce
howyea <yhynew@126.com>
2022-08-15 16:24:05 +0800

    remove useless code

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,8 +1,4 @@
 vim.o.background = "dark"
--- vim.g.tokyonight_style = "storm" -- day / night
--- 半透明
--- vim.g.tokyonight_transparent = true
--- vim.g.tokyonight_transparent_sidebar = true
 local colorscheme = "OceanicNext"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
